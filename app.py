7e3e171015f60986becb5bb97de3d193f51b37d7
Tanya <thetwtanya@gmail.com>
2022-04-30 13:23:19 +0800

    add textsendmessage

--- app.py
+++ app.py
@@ -59,7 +59,9 @@ def handle_message(event):
     elif '訂位' in msg:
         r = '您想訂位是嗎'
     
-
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
