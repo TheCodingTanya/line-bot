dc85796cff45422643bf12b0cd1c24625368db74
Tanya <thetwtanya@gmail.com>
2022-04-30 12:51:58 +0800

    sticker

--- app.py
+++ app.py
@@ -50,7 +50,10 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,       # token 權杖:回復訊息
-        TextSendMessage(text=r))
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+    ))
 
 
 if __name__ == "__main__":
